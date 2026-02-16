e11ff488f4c8b467f7bcc63ad36ae601e544bf9f
Davide Cannerozzi <tuaemail@example.com>
2026-02-16 13:03:33 +0100

    init project and add header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <header className="flex flex-col md:flex-row justify-between border-b-2 border-b-cyan-800 p-8">
+      <h1>Snippet Lint</h1>
+      <ul className="flex flex-col md:flex-row gap-8 md:gap-24 mt-12 md:mt-0">
+        <li>
+          <a href="https://github.com/DavideCannerozzi/Snippet-Lint">Github</a>
+        </li>
+        <li>
+          <a href="#">Documentation</a>
+        </li>
+      </ul>
+    </header>
+  );
+}
